6cbc6122d44d91da4bb663b6f332a8017b5faaf1
zerrougui salim <salim.zerrougui@umc.edu.dz>
2026-03-30 11:51:00 +0100

    new feature 1 second commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello world")
 print("This is a test of the main.py file.")
-
-print("new feature 1")
+print("This file is used to demonstrate how to run a Python script.")
+#stash comment fdghdfgdfg fdgdfgdfgfg
