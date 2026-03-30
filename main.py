0e14b032bd5a046fae7594f8d27ebcd1d9f9d3a1
zerrouguisalim-dotcom <zerrougui.salim@gmail.com>
2026-03-30 12:19:55 +0100

    commited by second user

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Hello world")
 print("This is a test of the main.py file.")
 print("This file is used to demonstrate how to run a Python script.")
 #stash comment fdghdfgdfg fdgdfgdfgfg
+
+
+prin(" this line was added by a second person")
